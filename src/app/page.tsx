3aa36b10bb0ecdf77ffda15f30d1f99329a159af
crimsonyasukie <amanciochristopher.official@gmail.com>
2024-11-18 09:14:09 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Link } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button"
+
+
+
+export default function Home() {
+  return (
+    <main>
+      Hello World
+    </main>
+  );
+}
